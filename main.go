6b3cebe3df252410688f6b3afb64835ed76d04aa
justalike <44633493+justalike@users.noreply.github.com>
2024-10-29 12:51:19 +0300

    Update main.go
    [+] Help command

--- main.go
+++ main.go
@@ -8,6 +8,38 @@ import (
 	"time"
 )
 
+func printHelp() {
+	fmt.Println(`Usage:
+  pngify [command] [options]
+
+Commands:
+  encode    Encode text or file data into a PNG image
+  decode    Decode a PNG image to retrieve the original data
+  help      Show this help message
+
+Options:
+
+Encode:
+  -t "text"   Text to encode into the PNG image
+  -f "file"   File to encode into the PNG image
+
+Decode:
+  -p "path"   Path to the PNG image to decode
+
+Examples:
+  Encode text:
+    pngify encode -t "Your text here"
+  
+  Encode a file:
+    pngify encode -f /path/to/file
+
+  Decode a file:
+    pngify decode -p /path/to/image.png
+
+Output:
+  An image named output.png will be created in the current directory for encoding.`)
+}
+
 func main() {
 	encodeCmd := flag.NewFlagSet("encode", flag.ExitOnError)
 	encodeText := encodeCmd.String("t", "", "Text to encode")
@@ -18,6 +50,7 @@ func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("A command must be specified.")
+		printHelp()
 		os.Exit(1)
 	}
 
@@ -29,7 +62,6 @@ func main() {
 		var img *image.Image
 
 		if *encodeText != "" {
-			// Encode text
 			img = image.NewImage([]byte(*encodeText))
 		} else if *encodeFile != "" {
 			content, err := os.ReadFile(*encodeFile)
@@ -39,17 +71,14 @@ func main() {
 			}
 
 			fmt.Println("Encoding file...")
-
 			img = image.NewImage(content)
 			img.MakeText([]byte("filename"), []byte(*encodeFile))
 		}
 
 		img.MakeImage()
 
-		break
 	case "decode":
 		_ = decodeCmd.Parse(os.Args[2:])
-
 		f, err := os.Open(*filePath)
 		defer f.Close()
 
@@ -59,29 +88,32 @@ func main() {
 		}
 
 		fmt.Println("Decoding image...")
-
 		data, fileName := image.Decode(f)
 
 		if fileName == "" {
 			fmt.Println(data)
 		} else {
-			// Save file
 			f, err := os.Create(fileName)
 			defer f.Close()
 
 			if err != nil {
-				panic("Couldn't create the output file.")
+				fmt.Println("Couldn't create the output file.")
+				os.Exit(1)
 			}
 
 			_, err = f.Write([]byte(data))
-
 			if err != nil {
-				panic("Couldn't create the output file.")
+				fmt.Println("Couldn't write data to the output file.")
+				os.Exit(1)
 			}
 		}
-		break
+
+	case "help", "--help", "-help":
+		printHelp()
+
 	default:
 		fmt.Println("Command not recognised")
+		printHelp()
 		os.Exit(1)
 	}
 
